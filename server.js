9ceab9cc1087100dc9410c889d5eaf48bdb050a1
Sagar-Rajput7 <sagar.rajput@harness.io>
2023-04-13 08:12:58 +0530

    added docker action points

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var cors = require("cors");
+const { spawn } = require("child_process");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,38 @@ app.use(
 );
 
 app.get("/", (req, res) => res.send("API Running"));
+//deploy app
+app.get("/deploy", (req, res) => {
+  const dockerDeploy = spawn("docker", [
+    "run",
+    "--platform",
+    "linux/amd64",
+    "-p",
+    "8081:5000",
+    "-d",
+    "flask",
+  ]);
+
+  dockerDeploy.stdout.on("data", (data) => {
+    console.log(data.toString());
+    res.write(data.toString());
+  });
+
+  dockerDeploy.stderr.on("data", (data) => {
+    console.error(data.toString());
+    res.write(data.toString());
+  });
+
+  dockerDeploy.on("close", (code) => {
+    if (code === 0) {
+      res.end("Docker container deployed successfully!");
+    } else {
+      res
+        .status(500)
+        .end(`Docker container deployment failed with code ${code}`);
+    }
+  });
+});
 
 // Define Routes
 app.use("/api/dataset", require("./routes/api/dataset"));
